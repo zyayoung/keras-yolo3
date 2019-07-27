e2f896de5b64f297974d856592d06c73ec3b96af
Zhang Yuang <1328410180@qq.com>
2019-07-27 12:07:34 +0800

    update

--- yolo.py
+++ yolo.py
@@ -5,10 +5,12 @@ Class definition of YOLO_v3 style detection model on image and video
 
 import colorsys
 import os
+# os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 from timeit import default_timer as timer
 import tensorflow as tf
 
 import numpy as np
+import tensorflow as tf
 from keras import backend as K
 from keras.models import load_model
 from keras.layers import Input
@@ -19,8 +21,43 @@ from yolo3.utils import letterbox_image
 import os
 from keras.utils import multi_gpu_model
 
-from munkres import Munkres
+from munkres import Munkres, DISALLOWED
 m = Munkres()
+
+def nms(dets, scores, thresh):
+    """
+    greedily select boxes with high confidence and overlap with current maximum <= thresh
+    rule out overlap >= thresh
+    :param dets: [[x1, y1, x2, y2 score]]
+    :param thresh: retain overlap < thresh
+    :return: indexes to keep
+    """
+    x1 = dets[:, 0]
+    y1 = dets[:, 1]
+    x2 = dets[:, 2]
+    y2 = dets[:, 3]
+
+    areas = (x2 - x1 + 1) * (y2 - y1 + 1)
+    order = scores.argsort()[::-1]
+
+    keep = np.zeros_like(scores, dtype=bool)
+    while order.size > 0:
+        i = order[0]
+        keep[i] = 1
+        xx1 = np.maximum(x1[i], x1[order[1:]])
+        yy1 = np.maximum(y1[i], y1[order[1:]])
+        xx2 = np.minimum(x2[i], x2[order[1:]])
+        yy2 = np.minimum(y2[i], y2[order[1:]])
+
+        w = np.maximum(0.0, xx2 - xx1 + 1)
+        h = np.maximum(0.0, yy2 - yy1 + 1)
+        inter = w * h
+        ovr = inter / (areas[i] + areas[order[1:]] - inter)
+
+        inds = np.where(ovr <= thresh)[0]
+        order = order[inds + 1]
+
+    return keep
 
 class YOLO(object):
     _defaults = {
@@ -28,7 +65,7 @@ class YOLO(object):
         "anchors_path": 'anchors.txt',
         "classes_path": 'classes.txt',
         "score" : 0.1,
-        "iou" : 0.45,
+        "iou" : 0.3,
         "model_image_size" : (608, 608),
         "gpu_num" : 1,
     }
@@ -48,9 +85,11 @@ class YOLO(object):
         self.sess = K.get_session()
         self.boxes, self.scores, self.classes = self.generate()
         self.prev_bbox = None
+        self.prev_velocity = None
+        self.prev_bbox_frame_cnt = None
         self.connections = []
         self.instances = []
-        self.instance_count = 0
+        self.instance_count = 1
         # print(self.boxes[0])
 
     def _get_class(self):
@@ -66,9 +105,9 @@ class YOLO(object):
             anchors = f.readline()
         anchors = [float(x) for x in anchors.split(',')]
         return np.array(anchors).reshape(-1, 2)
-
+    
     @staticmethod
-    def bbox_overlaps_py(boxes, query_boxes):
+    def bbox_overlap(boxes, query_boxes):
         """
         determine overlaps between boxes and query_boxes
         :param boxes: n * 4 bounding boxes
@@ -89,6 +128,26 @@ class YOLO(object):
                         all_area = float(box_area + query_box_area - iw * ih)
                         overlaps[n, k] = iw * ih / all_area
         return overlaps
+
+    @staticmethod
+    def bbox_distance(boxes, query_boxes, area):
+        """
+        determine distance between boxes and query_boxes
+        :param boxes: n * 4 bounding boxes
+        :param query_boxes: k * 4 bounding boxes
+        :return: distance: n * k overlaps
+        """
+        ax = (boxes[:,2]+boxes[:,0])/2
+        ay = (boxes[:,3]+boxes[:,1])/2
+        bx = (query_boxes[:,2]+query_boxes[:,0])/2
+        by = (query_boxes[:,3]+query_boxes[:,1])/2
+        distance = np.zeros((ax.shape[0], bx.shape[0]), dtype=np.float)
+        for i in range(ax.shape[0]):
+            for j in range(bx.shape[0]):
+                distance[i, j] = np.square(ax[i]-bx[j]) + np.square(ay[i]-by[j])
+        distance = np.sqrt(distance/area)
+        # distance /= distance.min()
+        return distance
 
     def generate(self):
         model_path = os.path.expanduser(self.model_path)
@@ -158,44 +217,91 @@ class YOLO(object):
 
         print('Found {} boxes for {}'.format(len(out_boxes), 'img'))
 
-        out_ids = {}
         
         connection = {}
+        out_ids = np.zeros(len(out_boxes), int)
+        frame_cnt = np.zeros(len(out_boxes))
+        out_velocity = np.zeros((len(out_boxes),4))
         if self.prev_bbox is not None:
-            matrix = self.bbox_overlaps_py(self.prev_bbox, out_boxes) + out_scores.T
-            matrix = 1/(np.asarray(matrix)+1e-6)
+            # matrix = 1/608*self.bbox_distance(self.prev_bbox, out_boxes, image.width*image.height) \
+            #     + 1/self.bbox_overlap(self.prev_bbox, out_boxes)\
+            matrix = 1/( \
+                self.bbox_overlap(self.prev_bbox+self.prev_velocity, out_boxes) + \
+                    np.exp(-61*self.bbox_distance(self.prev_bbox, out_boxes, image.width*image.height))+ \
+                    0)# - np.repeat(np.expand_dims(self.prev_bbox_frame_cnt, axis=1)/64, len(out_scores), axis=1) + 1/(out_scores.T+0.5)
+            #  - np.repeat(np.expand_dims(self.prev_bbox_frame_cnt, axis=1)/10, len(out_scores), axis=1)
+            #  + out_scores.T
             h, w = matrix.shape
+            matrix[matrix>1e6]=1e6
             _matrix = np.ones((max(h,w), max(h,w)))*1e6
             _matrix[:h, :w] = matrix.copy()
             # print(_matrix)
             indexes = m.compute(_matrix)
             total = 0
+            prev_mask = np.zeros(len(self.prev_bbox), dtype=bool)
             for row, column in indexes:
-                if row >= h or column >= w or matrix[row][column]>1e5:
+                if row >= h or column >= w or matrix[row][column]>10:
                     continue
                 value = matrix[row][column]
                 total += value
                 print(f'{row} -> {column} | {value}')
                 connection[row] = column
-                if self.instances and row in self.instances[-1].keys():
+                if self.instances and self.instances[-1][row] > 0:
+                    prev_mask[row] = True
                     out_ids[column] = self.instances[-1][row]
+                    out_velocity[column] = out_boxes[column] - self.prev_bbox[row]
+                    out_velocity[column] = 0.1 * out_velocity[column] + 0.9 * self.prev_velocity[row]
+                    out_boxes[column] = self.prev_bbox[row] + out_velocity[column]
+                    frame_cnt[column] = self.prev_bbox_frame_cnt[row] + 1
             print(f'total cost: {total}')
+
+            for index, box in enumerate(self.prev_bbox):
+                if not prev_mask[index] and self.prev_bbox_frame_cnt[index] > 5:
+                    out_boxes = np.concatenate([out_boxes, [box+self.prev_velocity[index]]], axis=0)
+                    frame_cnt = np.concatenate([frame_cnt, [self.prev_bbox_frame_cnt[index]]], axis=0)
+                    out_classes = np.concatenate([out_classes, [0]], axis=0)
+                    out_scores = np.concatenate([out_scores, [0]], axis=0)
+                    out_velocity = np.concatenate([out_velocity, [0.9*self.prev_velocity[index]]], axis=0)
+                    out_ids = np.concatenate([out_ids, [self.instances[-1][index]]], axis=0)
+
+            drop = (out_ids == 0)
+            keep = (out_ids > 0)
+            iou = self.bbox_overlap(out_boxes[drop], out_boxes[keep]).max(axis=1)
+            nms_keep = nms(out_boxes[drop], out_scores[drop], 0.3)
+            keep[np.where(drop)[0][(iou==0) & nms_keep]] = True
+            out_boxes = out_boxes[keep]
+            frame_cnt = frame_cnt[keep]
+            out_classes = out_classes[keep]
+            out_scores = out_scores[keep]
+            out_ids = out_ids[keep]
+            out_velocity = out_velocity[keep]
+        # else:
+        #     keep = nms(out_boxes, out_scores, 0.3)
+        #     out_boxes = out_boxes[keep]
+        #     frame_cnt = frame_cnt[keep]
+        #     out_classes = out_classes[keep]
+        #     out_scores = out_scores[keep]
+        #     out_ids = out_ids[keep]
+
         self.prev_bbox = out_boxes.copy()
+        self.prev_velocity = out_velocity.copy()
+        self.prev_bbox_frame_cnt = frame_cnt
 
         font = ImageFont.truetype(font='font/FiraMono-Medium.otf',
-                    size=np.floor(1e-2 * image.size[1] + 0.5).astype('int32'))
+                    size=np.floor(2e-2 * image.size[1] + 0.5).astype('int32'))
         thickness = (image.size[0] + image.size[1]) // 1000
 
         for i, c in list(enumerate(out_classes)):
             predicted_class = self.class_names[c]
             box = out_boxes[i]
             score = out_scores[i]
-            if i not in out_ids.keys():
+            if out_ids[i] == 0:
                 out_ids[i] = self.instance_count
                 self.instance_count += 1
             ins_id = out_ids[i]
-
-            label = '{} {:.2f}'.format(predicted_class, score)
+            if frame_cnt[i] < 3:
+                ins_id = 0
+            label = '{}'.format(ins_id)
             draw = ImageDraw.Draw(image)
             label_size = draw.textsize(label, font)
 
@@ -250,9 +356,7 @@ def detect_video(yolo, video_path, output_path=""):
     prev_time = timer()
 
     channal_diff = np.int32(video_fps)
-    history = {}
-    prev_bbox = None
-    sess = tf.Session()
+    history = []
     while True:
         return_value, frame = vid.read()
         if not return_value:
@@ -277,11 +381,11 @@ def detect_video(yolo, video_path, output_path=""):
             curr_fps = 0
         cv2.putText(result, text=fps, org=(3, 15), fontFace=cv2.FONT_HERSHEY_SIMPLEX,
                     fontScale=0.50, color=(255, 0, 0), thickness=2)
-        #cv2.namedWindow("result", cv2.WINDOW_NORMAL)
-        #cv2.imshow("result", result)
+        cv2.namedWindow("result", cv2.WINDOW_NORMAL)
+        cv2.imshow("result", result)
         if isOutput:
             out.write(result)
-        #if cv2.waitKey(1) & 0xFF == ord('q'):
-        #    break
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+           break
     yolo.close_session()
 
